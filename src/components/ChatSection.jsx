70341c8d62791d37e164de19cb5005bbed95a440
shreyas0924 <shreyasp0924@gmail.com>
2023-05-28 15:31:26 +0530

    fetched data pagination not done

--- src/components/ChatSection.jsx
+++ src/components/ChatSection.jsx
@@ -1,10 +1,37 @@
-import React from 'react'
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React, { useEffect, useState } from 'react'
+
+const url = process.env.REACT_APP_API_URL
 
 const ChatSection = () => {
+  const [chats, setChats] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(url)
+        const data = await response.json()
+        setChats(data.chats)
+      } catch (error) {
+        console.log('Error fetching data:', error)
+      }
+    }
+
+    fetchData()
+
+    console.log(chats)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <div>
-      <h1>Chat</h1>
-    
+      {chats.map((chat) => (
+        <div key={chat.id}>
+          <img src={chat.sender.image} alt='User Image' />
+          <p>{chat.message}</p>
+          <p>Sender ID: {chat.sender.user_id}</p>
+        </div>
+      ))}
     </div>
   )
 }
